90f1023e55985748340856a72d4bc93ce13bf259
Forslund, Beatrice I.B <b.forslund@student.fontys.nl>
2020-09-16 16:57:47 +0200

    Fixed test

---
+++ Online-workshop-beatrice/src/test/java/individual/project/BlaTest.java
@@ -0,0 +1,19 @@
+package individual.project;
+
+import org.junit.jupiter.api.Test;  
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple Dep.
+ */
+public class BlaTest
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testDep()
+    {
+        assertTrue(true);
+    }
+}
